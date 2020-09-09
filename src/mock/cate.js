0795c314ff6a5a48eebb33cee493074cf594600e
zhuqiao <zhuqiao@yidianling.com>
2020-09-09 14:54:00 +0800

    update：加载更多封装

---
+++ src/mock/cate.js
@@ -0,0 +1,8 @@
+export default{
+    code:'200',
+    data:{
+        list:['a','b','c'],
+        total:64,
+        totalPages:4
+    }
+}
--- src/page/demo.vue
+++ src/page/demo.vue
@@ -1,34 +1,39 @@
 <template>
-    <div id="box">
         <div class="box" v-infinite-scroll="load" infinite-scroll-disabled="disabled" >
             <ul class="list" >
-                <li v-for="(i,index) in list" class="list-item" :key="index">{{ i.noticeTitle }}</li>
+                <li v-for="(i,index) in list" class="list-item" :key="index">{{ i }}</li>
             </ul>
-            <p v-if="loading" style="margin-top:10px;" class="loading">
+            <p v-if="loading" class="loading">
                 <span></span>
             </p>
-            <p v-if="noMore" style="margin-top:10px;font-size:13px;color:#ccc">没有更多了</p>
+            <p v-if="noMore && !loading" class="noMoreText">没有更多了</p>
         </div>
-    </div>
 </template>
 
 <script>
+//加载更多，通过总页数据来判断
+
+import cate from '../mock/cate'
+
 export default {
     data() {
         return {
-            count: 0,//起始页数值为0
-            loading: false,
+            page: 1,//起始页数值为
+            loading: true,  //初始也在加载中
             totalPages: "",//取后端返回内容的总页数
             list: [] //后端返回的数组
         };
     },
     computed: {
+        //当
         noMore() {
             //当起始页数大于总页数时停止加载
-            return this.count >= this.totalPages - 1;
+            return this.page >= this.totalPages*1;
         },
+        //没有loading,没有更多就停止加载
         disabled() {
-            return this.loading || this.noMore;
+            // return this.loading || this.noMore;
+            return this.noMore;
         }
     },
     created() {
@@ -36,47 +41,46 @@ export default {
     },
     methods: {
         load() {
+            console.log('到底了');
+            //这里要加锁
+            if(this.loading){
+                return;
+            }
             //滑到底部时进行加载
             this.loading = true;
-            setTimeout(() => {
-                this.count += 1; //页数+1
-                this.getMessage(); //调用接口，此时页数+1，查询下一页数据
-            }, 2000);
+            this.page += 1; //页数+1
+            this.getMessage(); //调用接口，此时页数+1，查询下一页数据
         },
+        //数据请求
         getMessage() {
             let params = {
-                pageNumber: this.count,
+                pageNumber: this.page,
                 pageSize: 10 //每页查询条数
             };
-            this.$axios
-                .post(
-                    "https://接口",
-                    params
-                )
-                .then(res => {
-                    console.log(res);
-                    this.list = this.list.concat(res.data.body.content); //因为每次后端返回的都是数组，所以这边把数组拼接到一起
-                    this.totalPages = res.data.body.totalPages;
-                    this.loading = false;
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+
+            const paramsData = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`).join('&')
+            fetch(`http://testapi.ydl.com/api/information/cate?${paramsData}`).then(res=>{
+                return res.json();
+            }).then(res=>{
+                res=cate;
+                const { list, totalPages }=res.data;
+                this.list = this.list.concat(list); //因为每次后端返回的都是数组，所以这边把数组拼接到一起
+                this.totalPages = totalPages;
+                this.loading = false;
+            })
+
         }
     }
 };
 </script>
 
 <style scoped>
-#box{
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    overflow-y: auto;
-}
+
 .box {
-    width: 100%;
-    margin:  0 auto;
+    width: 400px;
+    height: 500px;
+    overflow-y: auto;
+    border: 2px solid cadetblue;
 }
 .list {
     padding: 0;
@@ -91,9 +95,13 @@ export default {
     list-style: none;
     padding: 0 1rem;
     box-sizing: border-box;
-    height: 70px;
-    line-height: 70px;
+    height: 250px;
+    line-height: 250px;
     border-bottom: 1px solid #e7e7e7;
+}
+.loading{
+    margin: 10px 0;
+    text-align: center;
 }
 .loading span {
     display: inline-block;
@@ -103,6 +111,12 @@ export default {
     border-left: transparent;
     animation: zhuan 0.5s linear infinite;
     border-radius: 50%;
+}
+.noMoreText{
+    margin:10px 0;
+    font-size:13px;
+    color:#ccc;
+    text-align: center;
 }
 @keyframes zhuan {
     0% {
